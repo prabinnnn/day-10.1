45279c8eb9001d06921a8e85143e5e436da46a7a
prabin <bhandariprabin402@gmail.com>
2023-12-26 22:23:47 +0545

    add e2

--- e2.js
+++ e2.js
@@ -1,8 +1,13 @@
-let map = new map();
-map.set("in", "india");
-map.set("usa", "untied state of america");
-map.set("np", "nepal");
-console.log(map);
-for (const [key, value] of map) {
-  console.log(key, ":-", value);
-}
+//let map = new map();
+//map.set("in", "india");
+//map.set("usa", "untied state of america");
+//map.set("np", "nepal");
+//console.log(map);
+//for (const [key, value] of map) {
+//console.log(key, ":-", value);
+//}
+
+const coding = ["prab", "rab", "hem", "jem"];
+coding.forEach(function (val) {
+  console.log(val);
+});
